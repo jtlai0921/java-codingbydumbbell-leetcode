a562694faa6908da95d77ad063fdb8eb17a692d2
Rick <rick8220602@gmail>
2019-01-09 13:53:43 +0800

    LongestSubstringWithoutRepeatingCharacters

--- src/Test.java
+++ src/Test.java
@@ -2,14 +2,5 @@ public class Test {
 
     public static void main(String[] args) {
 
-        // 相同的回傳 0
-        System.out.println("A".compareTo("A"));
-
-        // 不同，回傳首位不同字 Unicode 的差值
-        System.out.println("A".compareTo("C"));
-        System.out.println("AA".compareTo("AC"));
-
-        // 若前面全部相同，只有長度不同，回傳長度差異
-        System.out.println("我".compareTo("AAA"));
     }
 }
--- src/problems/LongestSubstringWithoutRepeatingCharacters.java
+++ src/problems/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,12 +1,12 @@
 package problems;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public static void main(String[] args) {
-        String string = "abcabcbb";
+        String string = "abcggf";
 
         int res = new LongestSubstringWithoutRepeatingCharacters()
                 .lengthOfLongestSubstring(string);
@@ -15,22 +15,29 @@ public class LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String s) {
 
+        // 如果是 null 或空值
         if (s == null || s.isEmpty()) return 0;
 
-        Map<Character, Integer> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
         int res = 0;
 
         // i 是目前的位置, j 則代表不重覆起點的位置
-        for (int i = 0, j = 0; i < s.length(); i++) {
-            if (map.containsKey(s.charAt(i))) // 如果遇到重覆的
-                // 則更新不重覆起點的位置, 要注意的是, 如果遇到不同字母皆重覆, 如 "abba", 則不重覆起點必須取最新, 即最大那個
-                j = Math.max(j, map.get(s.charAt(i)) + 1);
-
-            // 將字, 位置放入 map 中, 若有相同的, 則 i 被更新
-            map.put(s.charAt(i), i);
-
-            // 計算長度, 並保留最大值
-            res = Math.max(res, i - j + 1);
+        for (int i = 0, j = 0; i < s.length(); ) {
+            // 遇到重覆的字元
+            if (set.contains(s.charAt(i))) {
+                // 就把當前字元移除
+                set.remove(s.charAt(j));
+                // 起點位置往後移位
+                j++;
+            } else { // 不是重覆字元
+                // 將該字元放入 set 內
+                set.add(s.charAt(i));
+                // 比較是否為最長字串
+                res = Math.max(res, set.size());
+                // 跳到下一個位置
+                i++;
+            }
+            System.out.println(set);
         }
         return res;
     }
