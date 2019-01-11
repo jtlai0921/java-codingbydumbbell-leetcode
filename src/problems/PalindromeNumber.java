95ca33a50445bd53037d2fdceb93c1cdf4854ce5
Rick <rick8220602@gmail>
2019-01-11 14:11:14 +0800

    PalindromeNumber

--- src/problems/PalindromeNumber.java
+++ src/problems/PalindromeNumber.java
@@ -12,20 +12,23 @@ public class PalindromeNumber {
 
     public boolean isPalindrome(int x) {
 
+
+        System.out.println(new Integer(x).floatValue());
         // 如果 x < 0 就 return false
         if (x < 0) return false;
 
+        // 將 x 轉為 String
         String str = String.valueOf(x);
 
-        while (true) {
-
-            // 若長度小於 2, 則代表可能為空字串或單字
-            if (str.length() < 2) return true;
+        // 若長度小於 2, 則代表可能為空字串或單字
+        while (str.length() >= 2) {
 
             // 判斷字串的第一個是否等於最後一個, 沒有則 return false
             if (str.charAt(0) != str.charAt(str.length() - 1)) return false;
 
+            // 取去掉頭尾的字串
             str = str.substring(1, str.length() - 1);
         }
+        return true;
     }
 }
