de97b1399e66f12126c563606a517e459f6c4a8c
Rick <rick8220602@gmail.com>
2019-01-12 10:55:38 +0800

    Pow

--- src/problems/Pow.java
+++ src/problems/Pow.java
@@ -3,34 +3,24 @@ package problems;
 public class Pow {
 
     public static void main(String[] args) {
-        double d = new Pow().myPow(2, 10);
+        double d = new Pow().myPow(2, -1);
         System.out.println(d);
     }
 
-
     public double myPow(double x, int n) {
-        // 解決 -n 可能會超出 int 上限值的問題, 所以重刻一個方法
-        return clac(x, n);
-    }
+        double res = 1.0;
 
-    private double clac(double x, long n) {
+        // 每次除 2, 直到 i 等於 0
+        for (int i = n; i != 0; i /= 2) {
 
-        // 任何數的零次方皆為 0
-        if (n == 0) return 1;
+            // 第一次判斷時, n 可能為奇數, 所以要乘一次
+            if (i % 2 != 0) res *= x;
 
-        if (n > 0) {
-
-            // 用遞迴的方法, 最後一定會 n = 0
-            double res = clac(x, n / 2);
-
-            // 每次扣除兩個次方
-            res *= res;
-
-            // 如果有餘, 代表還要再補一個次方, 因為 0.5 在下一輪 n/2 就會被忽略
-            if (n % 2 != 0) res *= x;
-            return res;
-        } else // 負號轉換
-            return clac(1 / x, -n);
+            // 每次運算為平方
+            x *= x;
+        }
+        // 判斷 n 的正負號
+        return n < 0 ? 1 / res : res;
     }
 }
 
