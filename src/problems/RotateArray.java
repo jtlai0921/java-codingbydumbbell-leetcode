485a031f15cd40e9a2a8e1f34eda1c0fc3a1dc6f
Rick <rick8220602@gmail>
2019-01-14 12:01:41 +0800

    RotateArray

--- src/problems/RotateArray.java
+++ src/problems/RotateArray.java
@@ -5,7 +5,7 @@ public class RotateArray {
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int k = 3;
+        int k = 1;
 
         new RotateArray().rotate(nums, k);
 
@@ -16,9 +16,39 @@ public class RotateArray {
 
     public void rotate(int[] nums, int k) {
 
-        for(int x = 0; x <= nums.length-1; x++){
+        // 特殊情況排除
+        if (nums == null || nums.length <= 1) return;
 
-            nums[(x+k) % nums.length ] = nums[x];
+        // 求 k 除 陣列長度的餘數, 避免 k 超過陣列長度
+        k %= nums.length;
+
+        // 第一次翻轉
+        reverse(nums, 0, nums.length - 1);
+
+        // 第二次翻轉
+        reverse(nums, 0, k - 1);
+
+        // 第三次翻轉
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int s, int e) {
+
+        // s 為起始項, e 為末項, 若 s >= e, 則代表已經交錯, 也就是說已翻轉完畢
+        while (s < e) {
+
+            // 將 nums[s] 放到暫存空間
+            int temp = nums[s];
+
+            // 然後置換 nums[s] 的內容為 nums[e]
+            nums[s] = nums[e];
+
+            // 最後將 temp 的內容放到 nums[e] 完成一次的置換
+            nums[e] = temp;
+
+            // 往下一位, 別忘了 s++, e--
+            s++;
+            e--;
         }
     }
 }
