f60bf3fecdf544bf19463f107e9ee21ce5e00fdb
Rick <rick8220602@gmail>
2019-01-22 11:46:03 +0800

    LongestUncommonSubsequenceI

--- src/problems/LongestUncommonSubsequenceI.java
+++ src/problems/LongestUncommonSubsequenceI.java
@@ -9,6 +9,10 @@ public class LongestUncommonSubsequenceI {
     }
 
     public int findLUSlength(String a, String b) {
-        return a.equals(b) ? -1 : Math.max(a.length(), b.length());
+
+        // 如果兩字串相同，返回 -1
+        if (a.equals(b)) return -1;
+
+        return Math.max(a.length(), b.length());
     }
 }
