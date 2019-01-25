9e7b396396d7fdde637d17685a886033c8393842
Rick <rick8220602@gmail>
2019-01-25 10:18:08 +0800

    ReverseWordsInAString

--- src/problems/ReverseWordsInAString.java
+++ src/problems/ReverseWordsInAString.java
@@ -9,34 +9,12 @@ public class ReverseWordsInAString {
 
     public String reverseWords(String s) {
 
-//        String[] words = s.trim().split(" +");
-//        Collections.reverse(Arrays.asList(words));
-//        return String.join(" ", words);
-
-//        StringBuilder sb = new StringBuilder();
-//        for (String str : s.split(" +")) {
-//            sb.insert(0, str).insert(0, " ");
-//        }
-//        return sb.toString().trim();
-
-        String str = s.trim();
-        StringBuilder result = new StringBuilder(str.length());
-        int j = str.length();
-        int i = str.length() - 1;
-        while (i > 0) {
-            if (str.charAt(i) == ' ') {
-                result.append(str, i + 1, j);
-                result.append(" ");
-                i--;
-                while (str.charAt(i) == ' ') {
-                    i--;
-                }
-                j = i + 1;
-            } else {
-                i--;
-            }
+        StringBuilder sb = new StringBuilder();
+        // 將字串拆成單字, " +" 代表一個或以上的「空格字元」
+        for (String str : s.split(" +")) {
+            // 從前面插入單字
+            sb.insert(0, str).insert(0, " ");
         }
-        result.append(str, 0, j);
-        return result.toString();
+        return sb.toString().trim();
     }
 }
