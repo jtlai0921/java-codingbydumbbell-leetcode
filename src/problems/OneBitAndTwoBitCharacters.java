c04b36c6d7bc5b819e9752e7a18cf0932560a813
任柏勳 <rick8220602@gmail.com>
2019-02-03 00:19:15 +0800

    OneBitAndTwoBitCharacters

--- src/problems/OneBitAndTwoBitCharacters.java
+++ src/problems/OneBitAndTwoBitCharacters.java
@@ -6,28 +6,15 @@ public class OneBitAndTwoBitCharacters {
         int[] bits = {0, 0, 0};
         boolean res = new OneBitAndTwoBitCharacters().isOneBitCharacter(bits);
         System.out.println(res);
-
-        boolean test = new OneBitAndTwoBitCharacters().isOneBitCharacter2(bits);
-        System.out.println(test);
     }
-
 
     public boolean isOneBitCharacter(int[] bits) {
         int i = 0;
-        while (i < bits.length - 2) if (bits[i++] == 1) i++;
-        return bits[i] == 0;
-    }
-
-    public boolean isOneBitCharacter2(int[] bits) {
-        int len = bits.length;
-        if (len < 2) return true;
-        if (bits[len - 1] == 1) return false;
-        for (int i = len - 2; i >= 0; i--) {
-            if (bits[i] == 0) {
-                len -= (i + 1);
-                break;
-            }
+        while (i < bits.length - 2) {
+            // 代表 2-bit，移兩位
+            if (bits[i] == 1) i += 2;
+            else i++; // 代表 1-bit，移一位
         }
-        return len % 2 == 1;
+        return bits[i] == 0;
     }
 }
