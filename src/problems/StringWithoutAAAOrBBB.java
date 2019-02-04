0f5bc789da15f8bddd068f0da4ecf96105a44bc5
Rick <rick8220602@gmail>
2019-02-04 11:25:22 +0800

    StringWithoutAAAOrBBB

--- src/problems/StringWithoutAAAOrBBB.java
+++ src/problems/StringWithoutAAAOrBBB.java
@@ -11,14 +11,18 @@ public class StringWithoutAAAOrBBB {
     public String strWithout3a3b(int A, int B) {
         return (A > B) ? combinChar(A, B, 'a') : combinChar(B, A, 'b');
     }
+
     private String combinChar(int A, int B, char C) {
         StringBuilder sb = new StringBuilder(A + B);
         while (A-- > 0) {
+            // 先拼一次多的那個字元
             sb.append(C);
+            // 如果 A 是多的那方，則再拼一個
             if (A > B) {
                 sb.append(C);
                 A--;
             }
+            // 記得補上一個 B
             if (B-- > 0) sb.append((char) ('a' + 'b' - C));
         }
         return sb.toString();
