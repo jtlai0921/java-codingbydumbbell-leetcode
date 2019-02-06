74b1fee2e1c13b6dc19965a3aa0979bf74a92e7a
任柏勳 <rick8220602@gmail.com>
2019-02-06 20:04:34 +0800

    PowerOfTwo

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,8 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(1|1);
-        System.out.println(0&1);
-    }
+        int i = 0x55555555;
 
-    public int reverseBits(int n) {
-        int out = 0;
-        for (int i = 0; i < 32; i++) {
-            out = out << 1;
-            int t = n & 1;
-            out += t;
-            n = n >> 1;
-        }
-        return out;
+        System.out.println(Integer.toBinaryString(i & 16));
     }
 }
--- src/problems/PowerOfThree.java
+++ src/problems/PowerOfThree.java
@@ -3,12 +3,20 @@ package problems;
 public class PowerOfThree {
 
     public static void main(String[] args) {
-        int n = 27;
+        int n = 488;
         boolean res = new PowerOfThree().isPowerOfThree(n);
         System.out.println(res);
     }
 
     public boolean isPowerOfThree(int n) {
-        return (n > 0 && Math.pow(3, 19) % n == 0);
+        if (n == 0) return false;
+
+        // 如果最後等於 1，就代表它是 3 的指數
+        while (n != 1) {
+            if (n % 3 == 0) n /= 3;
+                // 不能被 3 整除就一定不是 3 的指數
+            else return false;
+        }
+        return true;
     }
 }
