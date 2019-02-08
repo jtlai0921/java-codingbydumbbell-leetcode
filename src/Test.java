a7ec49679e2d21add7543aea92c23a5eaf2e460e
任柏勳 <rick8220602@gmail.com>
2019-02-09 01:32:22 +0800

    StringToIntegerAtoi

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,32 @@
 public class Test {
 
     public static void main(String[] args) {
-        int i = 0x55555555;
+        long value = 2147483648L;
+        int sign = -1;
+        System.out.println("test1:");
+        System.out.println(test1(value, sign));
+        System.out.println("test2:");
+        System.out.println(test2(value, sign));
+    }
 
-        System.out.println(Integer.toBinaryString(i & 16));
+    private static int test1(long value, int sign) {
+        if (value > Integer.MAX_VALUE) {
+            System.out.println("path: A");
+            return sign > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        }
+        System.out.println("path: B");
+        return (int) (sign * value);
+    }
+
+    private static int test2(long value, int sign) {
+        if (sign > 0 && value > Integer.MAX_VALUE) {
+            System.out.println("path: A");
+            return Integer.MAX_VALUE;
+        } else if (sign < 0 && -value < Integer.MIN_VALUE) {
+            System.out.println("path: B");
+            return Integer.MIN_VALUE;
+        }
+        System.out.println("path: C");
+        return (int) (sign * value);
     }
 }
