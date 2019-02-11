5620e7913dc7506cfbe397cd3ca631adbf8b073e
任柏勳 <rick8220602@gmail.com>
2019-02-11 23:18:42 +0800

    IsomorphicStrings

--- src/Test.java
+++ src/Test.java
@@ -1,42 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
 
     public static void main(String[] args) {
-        long value = 2147483648L;
-        int sign = -1;
-        System.out.println("test1:");
-        System.out.println(test1(value, sign));
-        System.out.println("test2:");
-        System.out.println(test2(value, sign));
-    }
 
-    private static int test1(long value, int sign) {
-        if (value > Integer.MAX_VALUE) {
-            System.out.println("path: A");
-            return sign > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-        }
-        System.out.println("path: B");
-        return (int) (sign * value);
-    }
+        Map<String, String> map = new HashMap<String, String>();
+        String p1 = map.put("11", "22");
+        System.out.println("p1:" + p1);
 
-    private static int test2(long value, int sign) {
-        if (sign > 0 && value > Integer.MAX_VALUE) {
-            System.out.println("path: A");
-            return Integer.MAX_VALUE;
-        } else if (sign < 0 && -value < Integer.MIN_VALUE) {
-            System.out.println("path: B");
-            return Integer.MIN_VALUE;
-        }
-        System.out.println("path: C");
-        return (int) (sign * value);
+        String p2 = map.put("33", "44");
+        System.out.println("p2:" + p2);
+
+        String value1 = map.get("11");
+        System.out.println("value1:" + value1);
+
+        String p3 = map.put("11", "44");
+        System.out.println("p3：" + p3);
+
+        String value2 = map.get("11");
+        System.out.println("value2:" + value2);
     }
 }
-/*
-public String longestCommonPrefix1(String[] strs) {
-        if (strs.length == 0) return "";
-        String pre = strs[0];
-        for (int i = 1; i < strs.length; i++)
-            while(strs[i].indexOf(pre) != 0)
-                pre = pre.substring(0,pre.length()-1);
-        return pre;
-    }
- */
+
