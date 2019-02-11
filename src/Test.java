db0434f55c40a5158853a3dc2172dbc927485b6f
任柏勳 <rick8220602@gmail.com>
2019-02-11 23:19:42 +0800

    IsomorphicStrings

--- src/Test.java
+++ src/Test.java
@@ -1,25 +1,7 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Test {
 
     public static void main(String[] args) {
 
-        Map<String, String> map = new HashMap<String, String>();
-        String p1 = map.put("11", "22");
-        System.out.println("p1:" + p1);
-
-        String p2 = map.put("33", "44");
-        System.out.println("p2:" + p2);
-
-        String value1 = map.get("11");
-        System.out.println("value1:" + value1);
-
-        String p3 = map.put("11", "44");
-        System.out.println("p3：" + p3);
-
-        String value2 = map.get("11");
-        System.out.println("value2:" + value2);
     }
 }
 
