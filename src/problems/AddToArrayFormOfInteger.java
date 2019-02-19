bb393db39e03c89d35705300c70064f23924f807
Rick <rick8220602@gmail>
2019-02-19 09:16:23 +0800

    RansomNote

---
+++ src/problems/AddToArrayFormOfInteger.java
@@ -0,0 +1,21 @@
+package problems;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class AddToArrayFormOfInteger {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> addToArrayForm(int[] A, int K) {
+
+        List<Integer> res = new ArrayList<>();
+        while (K / 10 != 0) {
+
+
+        }
+        return res;
+    }
+}
