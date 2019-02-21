c6463bc4c62f0b61bda6df5e08c5c23c0618f6b4
Rick <rick8220602@gmail>
2019-02-21 09:40:55 +0800

    AddToArrayFormOfInteger

--- src/problems/AddToArrayFormOfInteger.java
+++ src/problems/AddToArrayFormOfInteger.java
@@ -1,25 +1,27 @@
 package problems;
 
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.ArrayList;
 
 public class AddToArrayFormOfInteger {
 
     public static void main(String[] args) {
-        int A[] = {2, 1, 5}, K = 806;
+        int A[] = {9, 9, 9, 9, 9, 9, 9, 9, 9, 9}, K = 1;
         List<Integer> res = new AddToArrayFormOfInteger().addToArrayForm(A, K);
+        System.out.println(res);
     }
 
     public List<Integer> addToArrayForm(int[] A, int K) {
-
-        String s = Arrays.toString(A);
-        List<Integer> res = new ArrayList<>();
-        while (K / 10 != 0) {
-
-
+        List<Integer> res = new LinkedList<>();
+        int i = A.length - 1, carry = 0;
+        while (i >= 0 || K > 0 || carry > 0) {
+            int a = i >= 0 ? A[i--] : 0;
+            int b = K % 10;
+            K = K / 10;
+            int sum = a + b + carry;
+            carry = sum / 10;
+            res.add(0, sum % 10);
         }
         return res;
-
     }
 }
