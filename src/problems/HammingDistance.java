82d455bad4a96bb1803371ef8388729b07659a9c
任柏勳 <rick8220602@gmail.com>
2019-03-12 19:34:56 +0800

    HammingDistance

---
+++ src/problems/HammingDistance.java
@@ -0,0 +1,17 @@
+package problems;
+
+
+public class HammingDistance {
+
+    public static void main(String[] args) {
+        int x = 93, y = 73;
+        int res = new HammingDistance().hammingDistance(x, y);
+        System.out.println(res);
+    }
+
+    public int hammingDistance(int x, int y) {
+        System.out.println(Integer.toBinaryString(x));
+        System.out.println(Integer.toBinaryString(y));
+        return Integer.bitCount(x ^ y);
+    }
+}
