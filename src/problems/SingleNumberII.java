5b5418e4d8c56b76b05316e09333eee23ff2113a
Rick <rick8220602@gmail>
2019-03-15 23:03:59 +0800

    SingleNumberII

--- src/problems/SingleNumberII.java
+++ src/problems/SingleNumberII.java
@@ -10,17 +10,11 @@ public class SingleNumberII {
 
     public int singleNumber(int[] nums) {
         int ans = 0;
-        for (int i = 0; i < 32; i++) {
-            int sum = 0;
+        for (int i = 0, sum = 0; i < 32; i++, sum = 0) {
             for (int j = 0; j < nums.length; j++) {
-                if (((nums[j] >> i) & 1) == 1) {
-                    sum++;
-                    sum %= 3;
-                }
+                if (((nums[j] >> i) & 1) == 1) sum = ++sum % 3;
             }
-            if (sum != 0) {
-                ans |= sum << i;
-            }
+            if (sum != 0) ans |= sum << i;
         }
         return ans;
     }
