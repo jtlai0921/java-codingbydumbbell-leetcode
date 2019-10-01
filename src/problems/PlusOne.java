a1de81fe1fcece2b51d5a356a7cdd5aa64102a3f
Rick <rick8220602@gmail.com>
2019-10-01 15:53:36 +0800

    renew 1/1

--- src/problems/PlusOne.java
+++ src/problems/PlusOne.java
@@ -9,19 +9,12 @@ public class PlusOne {
     }
 
     public int[] plusOne(int[] digits) {
-
-        // 從個位數開始判斷起
         for (int i = digits.length - 1; i >= 0; i--) {
-
-            // 如果當前位數不是 9，就直接 +1 回傳
             if (digits[i] != 9) {
                 digits[i]++;
                 return digits;
-            } else // 如果當前位數是 9，就直接補成 0，然後繼續判斷下一位
-                digits[i] = 0;
+            } else digits[i] = 0;
         }
-
-        // 若程式碼執行到這行代表，該數字的所有位數皆為 9，就回傳下一位為 1，其他為 0 的陣列
         digits = new int[digits.length + 1];
         digits[0] = 1;
         return digits;
